07705782e1638b1e28d8daf8b21b509ca16d56ff
Lucas Gontijo da Cunha <lucasgontijoo470@gmail.com>
2026-08-06 17:42:07 -0300

    refactor: improve code formatting and readability in Program.java

--- aula97/src/application/Program.java
+++ aula97/src/application/Program.java
@@ -8,22 +8,25 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		int N = sc.nextInt();
 		Product[] produtos = new Product[N];
 		Product produto = new Product();
 		double soma = 0.0, media = 0.0;
-		
-		for(int i = 0; i<produtos.length; i++) {
+
+		for (int i = 0; i < produtos.length; i++) {
 			produto.nome = sc.next();
 			produto.preco = sc.nextDouble();
-			
+
 			produtos[i] = produto;
 			soma += produto.preco;
 		}
-		
-		media=soma/N;
+
+		media = soma / N;
 		System.out.printf("AVERAGE PRICE = %.2f%n", media);
+
+		sc.close();
+		;
 
 	}
 }
